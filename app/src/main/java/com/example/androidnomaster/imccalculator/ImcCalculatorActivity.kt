77f08ce5c759d5ca440096b84567fd3603088562
A98PH <122514691+A98PH@users.noreply.github.com>
2023-04-12 22:20:09 -0500

    Initial commit

--- app/src/main/java/com/example/androidnomaster/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/androidnomaster/imccalculator/ImcCalculatorActivity.kt
@@ -2,11 +2,56 @@ package com.example.androidnomaster.imccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.androidnomaster.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = true
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+        initComponents()
+        initListeners()
+        initUI()
     }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.btnMan)
+        viewFemale = findViewById(R.id.btnWoman)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener{setGenerColor()}
+        viewFemale.setOnClickListener{setGenerColor()}
+    }
+
+    private fun setGenerColor() {
+
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+
+    }
+
+    private fun getBackgroundColor(isSelectedComponent:Boolean) : Int {
+
+        val colorReference = if(isSelectedComponent) {
+            R.color.background_component_selected
+        }else {
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
 }
